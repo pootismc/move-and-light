14080c07d57ee98040473b52d6b72a6d71bebe08
Fabrizio Franceschetti <fabrizio.franceschetti00@gmail.com>
2022-03-01 00:31:17 +0100

    changed .env port

--- src/app.js
+++ src/app.js
@@ -10,8 +10,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '../public'))); 
 app.use('/', indexRouter);
 
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
 
-app.listen (3000, function(){
-    console.log("El servidor esta corriendo en el puerto 3000")
-})
+// app.listen (3000, function(){
+//     console.log("El servidor esta corriendo en el puerto 3000")
+// })
+
+app.listen(process.env.PORT || 8080);
