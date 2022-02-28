c080a45b11c09eaaddf842f6d0044e8fb07b032e
Fabrizio Franceschetti <fabrizio.franceschetti00@gmail.com>
2022-03-01 00:20:28 +0100

    trying

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const express = require ('express');
 const app = express();
 const path = require ('path');
 const indexRouter = require ('./routers/index');
+const session = require('express-session');
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static (path.join(__dirname, '../public')));
+app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, 'views')); 
+app.use(express.static(path.join(__dirname, '../public'))); 
 app.use('/', indexRouter);
 
 app.listen (3000, function(){
